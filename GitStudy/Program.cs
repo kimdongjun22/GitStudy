1529287da7fec12d07c9ab3360b7c03089766dc6
kimdongjun22 <dkdl01028@icloud.com>
2025-03-20 12:29:29 +0900

    NEW
    수정된 ㅂ주얼스튜디오

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("이름을 입력해주시요: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("입력하신 이름은{0}입니다:", input);
         }
     }
 }
